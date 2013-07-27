0bbf96882ade93fa360559b3c75ba7606f439e40
Darren Cauthon <darren@cauthon.com>
2013-07-26 21:58:17 -0500

    Initial commit.

---
+++ lib/starting_blocks/growl.rb
@@ -0,0 +1,7 @@
+require "starting_blocks/growl/version"
+
+module StartingBlocks
+  module Growl
+    # Your code goes here...
+  end
+end
---
+++ lib/starting_blocks/growl/version.rb
@@ -0,0 +1,5 @@
+module StartingBlocks
+  module Growl
+    VERSION = "0.0.1"
+  end
+end
